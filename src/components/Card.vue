75e5704dbd5d13609da91cdd35d0f6e0a5ee647e
Lizzetee <lizzete.gleez@gmail.com>
2025-05-03 01:35:42 -0600

    pagina fin

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,54 +1,51 @@
 <template>
-  <div class="row mt-4">
-    <div class="col-md-4">
-      <div class="card" style="width: 16rem">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/8793/8793961.png"
-          class="card-img-top"
-          alt="imagen"
-        />
-        <div class="card-body text-center">
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <button class="btn btn-warning">MÁS INFROMACIÓN</button>
+  <div class="container mt-5">
+    <div class="row justify-content-center g-4">
+      <div class="col-md-4 d-flex justify-content-center">
+        <div class="card" style="width: 16rem">
+          <img
+            src="https://img.freepik.com/vecteurs-premium/style-icone-etudiant-conception-vectorielle_1134108-151921.jpg"
+            class="card-img-top"
+            alt="imagen"
+          />
+          <div class="card-body text-center">
+            <p class="card-text">
+              En este apartado podrás conocer todos los documentos que deberás subir a tu expediente
+            </p>
+            <button class="btn btn-warning">RESIDENTES</button>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  <div class="row mt-4">
-    <div class="col-md-4">
-      <div class="card" style="width: 16rem">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/8793/8793961.png"
-          class="card-img-top"
-          alt="..."
-        />
-        <div class="card-body text-center">
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <button class="btn btn-warning">MÁS INFROMACIÓN</button>
+
+      <div class="col-md-4 d-flex justify-content-center">
+        <div class="card" style="width: 16rem">
+          <img
+            src="https://tbcdn.talentbrew.com/company/34155/31939/content/MicrosoftTeams_image__4__8ApGSD.png"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body text-center">
+            <p class="card-text">
+              Aquí podrás conocer los detalles de cada asesor, áreas de especialización y proyectos realizados
+            </p>
+            <button class="btn btn-warning">ASESORES</button>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  <div class="row mt-4">
-    <div class="col-md-4">
-      <div class="card" style="width: 16rem">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/8793/8793961.png"
-          class="card-img-top"
-          alt="..."
-        />
-        <div class="card-body text-center">
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <button class="btn btn-warning">MÁS INFROMACIÓN</button>
+
+      <div class="col-md-4 d-flex justify-content-center">
+        <div class="card" style="width: 16rem">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/5660/5660189.png"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body text-center">
+            <p class="card-text">
+              Conoce las preguntas más frecuentes y el departamento de vinculación resolverá tus dudas
+            </p>
+            <button class="btn btn-warning">VINCULACIÓN</button>
+          </div>
         </div>
       </div>
     </div>
